7801d9811f56d60e04becd01bb824ca8931328d0
INNOVINATI <maximilian.wolf@innovinati.com>
2020-12-08 17:06:19 +0100

    Added router config to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,11 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  // GitHub Pages deployment
+  router: {
+    base: '/scrapretty/'
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - scrapretty',
